9664fc42a34e63efd104422f51de9380233e8575
Your Name <you@example.com>
2025-10-20 20:41:43 +0300

    Update base URL and simplify deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/GeoNexis/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
